1ceada401aa0dfff6bc78ec52157d0894ae4c0a8
Temi <temi.sh14@gmail.com>
2026-04-16 22:46:40 +0100

    half ran rnn

--- src/train/poisson_nn/nn_main.py
+++ src/train/poisson_nn/nn_main.py
@@ -409,7 +409,6 @@ def fit_poisson_nn(
         start = time.time()
         mp = best_params[cell]["model_params"]
         tp = best_params[cell]["trainer_params"]
-        # tp["epochs"] = max(10, epochs // 3)  # Override epochs for final training
 
         def final_train_fn(model, Xtr_c, ytr_c, Xv_c, yv_c):
             trainer = PoissonTrainer(**tp)
